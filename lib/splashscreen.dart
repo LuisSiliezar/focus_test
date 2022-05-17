c60a6b029708a17f3c2f0b84cd45157e311dbf05
LuisSiliezar <66377843+LuisSiliezar@users.noreply.github.com>
2022-05-17 17:59:24 -0600

    Lottie Animation Splash

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 300,
+                width: 300,
+                child: Lottie.asset('assets/splash_loader.json'),
+              ),
+              const Text(
+                "Loading...",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              )
+            ],
+          )),
     );
   }
 }
